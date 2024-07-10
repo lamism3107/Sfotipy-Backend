6cea5ad576c650b62ecefb66112dde9f09cfd10a
unknown <lamtx.3107@gmail.com>
2024-07-10 14:36:12 +0700

    update

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
     // origin: "http://localhost:3000",
-    origin: "https://sfotipy-frontend.vercel.app/", //Chan tat ca cac domain khac ngoai domain nay
+    origin: "https://sfotipy-frontend.vercel.app", //Chan tat ca cac domain khac ngoai domain nay
     credentials: true, //Để bật cookie HTTP qua CORS
   })
 );
